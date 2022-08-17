0d73259aba37e0be66046002673d8b9f9689e8af
Bo Fehlan <bo.fehlan@gmail.com>
2022-08-16 23:33:59 -0700

    better msg system, uses std::variant

--- examples/indev.h
+++ examples/indev.h
@@ -3,6 +3,8 @@
 
 #include "yagi/util/log.h"
 #include "yagi/util/rnd.h"
+#include "fmt/format.h"
+#include "fmt/ostream.h"
 #include <any>
 #include <functional>
 #include <unordered_map>
@@ -13,109 +15,122 @@
 template<class ...Ts> struct overloaded : Ts... { using Ts::operator()...; };
 template<class ...Ts> overloaded(Ts...) -> overloaded<Ts...>;
 
+namespace yagi::msg {
+
 enum class MsgType {
   AEvent,
   BEvent,
   CEvent
 };
 
-template<MsgType>
-struct PayloadMap;
+std::ostream &operator<<(std::ostream &out, MsgType value) {
+  std::string s;
+#define STRINGIFY(p) case (p): s = #p; break;
+  switch (value) {
+    using
+    enum MsgType;
+    STRINGIFY(AEvent)
+    STRINGIFY(BEvent)
+    STRINGIFY(CEvent)
+  }
+#undef STRINGIFY
+  return out << s;
+}
 
-#define DEFINE_MSG_PAYLOAD(msg, ...)           \
-  template<> struct PayloadMap<MsgType::msg> { \
-    using type = struct msg ## Pay {           \
-      __VA_ARGS__                              \
-    };                                         \
+template<MsgType>
+struct MsgMap;
+
+#define DEFINE_MSG(t, ...)               \
+  struct t {                             \
+    __VA_ARGS__                          \
+  };                                     \
+  template<> struct MsgMap<MsgType::t> { \
+    using type = t;                      \
   };
 
-DEFINE_MSG_PAYLOAD(AEvent,
+DEFINE_MSG(AEvent,
   int a;
   int b;
 )
 
-DEFINE_MSG_PAYLOAD(BEvent,
+DEFINE_MSG(BEvent,
   int a;
   std::string s;
 )
 
-DEFINE_MSG_PAYLOAD(CEvent,
+DEFINE_MSG(CEvent,
   double a;
   double b;
   double c;
 )
 
-#undef DEFINE_MSG_PAYLOAD
+#undef DEFINE_MSG
 
 struct Msg {
-  const MsgType type;
-  std::any payload;
+  MsgType type;
+  std::variant<AEvent, BEvent, CEvent> data;
 };
 
 using MsgFunc = std::function<void(const Msg &)>;
 
-class Messenger {
+class Bus {
 public:
-  static Messenger &get_instance();
+  static std::string create_id(const MsgFunc &&f);
 
-  std::string create_id(const MsgFunc &&f);
+  static void subscribe(const std::string &id, const MsgType &type);
 
-  void subscribe(const std::string &id, const MsgType &type);
+  template<MsgType T, typename... Args>
+  static void send(const Args &...args);
 
-  template <MsgType T, typename... Args>
-  void send(const Args &...args);
-
-  void poll(const std::string &id);
-
-  template<MsgType T>
-  static auto extract_msg_payload(const std::any &payload);
+  static void poll(const std::string &id);
 
 private:
-  Messenger() = default;
+  Bus() = default;
+
+  static Bus &get_instance_();
 
   std::unordered_map<std::string, MsgFunc> funcs_{};
   std::unordered_map<MsgType, std::unordered_set<std::string>> subscriptions_{};
   std::unordered_map<std::string, std::queue<Msg>> queues_{};
 };
 
-Messenger &Messenger::get_instance() {
-  static auto instance = Messenger();
-
-  return instance;
-}
-
-std::string Messenger::create_id(const MsgFunc &&f) {
+std::string Bus::create_id(const MsgFunc &&f) {
   std::string id;
   do {
     id = rnd::base58(11);
-  } while (funcs_.contains(id));
+  } while (get_instance_().funcs_.contains(id));
 
-  funcs_[id] = f;
-  queues_[id] = std::queue<Msg>();
+  get_instance_().funcs_[id] = f;
+  get_instance_().queues_[id] = std::queue<Msg>();
 
   return id;
 }
 
-void Messenger::subscribe(const std::string &id, const MsgType &type) {
-  subscriptions_[type].insert(id);
+void Bus::subscribe(const std::string &id, const MsgType &type) {
+  get_instance_().subscriptions_[type].insert(id);
 }
 
-template <MsgType T, typename... Args>
-void Messenger::send(const Args &...args) {
-  for (const auto &id : subscriptions_[T])
-    queues_[id].push(Msg{T, typename PayloadMap<T>::type{args...}});
+template<MsgType T, typename... Args>
+void Bus::send(const Args &...args) {
+  for (const auto &id: get_instance_().subscriptions_[T])
+    get_instance_().queues_[id].push(Msg{T, typename MsgMap<T>::type{args...}});
 }
 
-void Messenger::poll(const std::string &id) {
-  while (!queues_[id].empty()) {
-    funcs_[id](queues_[id].front());
-    queues_[id].pop();
+void Bus::poll(const std::string &id) {
+  while (!get_instance_().queues_[id].empty()) {
+    get_instance_().funcs_[id](get_instance_().queues_[id].front());
+    get_instance_().queues_[id].pop();
   }
 }
 
-template<MsgType T>
-auto Messenger::extract_msg_payload(const std::any &payload) {
-  return std::any_cast<typename PayloadMap<T>::type>(payload);
+Bus &Bus::get_instance_() {
+  static auto instance = Bus();
+
+  return instance;
 }
+
+} // namespace yagi::msg
+
+template<> struct fmt::formatter<yagi::msg::MsgType> : ostream_formatter {};
 
 #endif //YAGI_EXAMPLES_INDEV_H
