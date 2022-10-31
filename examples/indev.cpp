0146fda4a3ef6c9bca9032e21777aa65a6f2c996
beauregardes <beauregardes@users.noreply.github.com>
2022-10-31 14:27:25 -0700

    better msg system, uses std::variant

--- examples/indev.cpp
+++ examples/indev.cpp
@@ -1,58 +1,33 @@
 #include "indev.h"
 
 int main(int, char *[]) {
-  auto id1 = Messenger::get_instance().create_id([](const Msg &msg) {
-    switch (msg.type) {
-      case MsgType::AEvent: {
-        auto pay = Messenger::extract_msg_payload<MsgType::AEvent>(msg.payload);
-        fmt::print("id1 | A: {}, {}\n", pay.a, pay.b);
-      }
-        break;
+  using namespace yagi;
 
-      case MsgType::BEvent: {
-        auto pay = Messenger::extract_msg_payload<MsgType::BEvent>(msg.payload);
-        fmt::print("id1 | B: {}, {}\n", pay.a, pay.s);
-      }
-        break;
-
-      case MsgType::CEvent: {
-        auto pay = Messenger::extract_msg_payload<MsgType::CEvent>(msg.payload);
-        fmt::print("id1 | C: {}, {}, {}\n", pay.a, pay.b, pay.c);
-      }
-        break;
-    }
+  auto id1 = msg::Bus::create_id([](const msg::Msg &msg) {
+    std::visit(overloaded {
+        [&](const msg::AEvent &e) { fmt::print("id1 | A: {}, {}\n", e.a, e.b); },
+        [&](const msg::BEvent &e) { fmt::print("id1 | B: {}, {}\n", e.a, e.s); },
+        [&](const auto &e)        { YAGI_LOG_WARN("Unhandled event {}", msg.type); },
+    }, msg.data);
   });
-  Messenger::get_instance().subscribe(id1, MsgType::AEvent);
-  Messenger::get_instance().subscribe(id1, MsgType::BEvent);
+  msg::Bus::subscribe(id1, msg::MsgType::AEvent);
+  msg::Bus::subscribe(id1, msg::MsgType::BEvent);
 
-  auto id2 = Messenger::get_instance().create_id([](const Msg &msg) {
-    switch (msg.type) {
-      case MsgType::AEvent: {
-        auto pay = Messenger::extract_msg_payload<MsgType::AEvent>(msg.payload);
-        fmt::print("id2 | A: {}, {}\n", pay.a, pay.b);
-      }
-        break;
-
-      case MsgType::BEvent: {
-        auto pay = Messenger::extract_msg_payload<MsgType::BEvent>(msg.payload);
-        fmt::print("id2 | B: {}, {}\n", pay.a, pay.s);
-      }
-        break;
-
-      case MsgType::CEvent: {
-        auto pay = Messenger::extract_msg_payload<MsgType::CEvent>(msg.payload);
-        fmt::print("id2 | C: {}, {}, {}\n", pay.a, pay.b, pay.c);
-      }
-        break;
-    }
+  auto id2 = msg::Bus::create_id([](const msg::Msg &msg) {
+    std::visit(overloaded {
+        [&](const msg::AEvent &e) { fmt::print("id2 | A: {}, {}\n", e.a, e.b); },
+        [&](const msg::CEvent &e) { fmt::print("id2 | C: {}, {}, {}\n", e.a, e.b, e.c); },
+        [&](const auto &e)        { YAGI_LOG_WARN("Unhandled event {}", msg.type); },
+    }, msg.data);
   });
-  Messenger::get_instance().subscribe(id2, MsgType::AEvent);
-  Messenger::get_instance().subscribe(id2, MsgType::CEvent);
+  msg::Bus::subscribe(id2, msg::MsgType::AEvent);
+  msg::Bus::subscribe(id2, msg::MsgType::CEvent);
 
-  Messenger::get_instance().send<MsgType::AEvent>(1, 2);
-  Messenger::get_instance().send<MsgType::BEvent>(1, "Hello");
-  Messenger::get_instance().send<MsgType::CEvent>(1.1, 2.2, 3.3);
+  msg::Bus::send<msg::MsgType::AEvent>(1, 2);
+  msg::Bus::send<msg::MsgType::BEvent>(1, "Hello");
+  msg::Bus::send<msg::MsgType::CEvent>(1.1, 2.2, 3.3);
 
-  Messenger::get_instance().poll(id1);
-  Messenger::get_instance().poll(id2);
+  msg::Bus::poll(id1);
+  msg::Bus::poll(id2);
 }
+
