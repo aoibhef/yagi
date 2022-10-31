71332140fda11eac1e4cca3240570412535f54f5
beauregardes <beauregardes@users.noreply.github.com>
2022-10-31 14:27:23 -0700

    more submodules

--- examples/indev.cpp
+++ examples/indev.cpp
@@ -1,6 +1,13 @@
 #include "yagi/msg/bus.h"
+#include "yagi/util/rnd.h"
 #include "fmt/format.h"
+#include "spdlog/spdlog.h"
 
 int main(int, char *[]) {
-  fmt::print("2 + 2 = {}\n", yagi::add(2, 2));
+  spdlog::set_level(spdlog::level::debug);
+
+  rnd::debug_show_seed();
+
+  for (int i = 0; i < 100; i++)
+    fmt::print("{}\n", rnd::base58(11));
 }
